d889603bf2b80e3d1fe5d54b6c23ca559622630e
Geuthur <geuthur@users.noreply.github.com>
2024-09-23 09:44:26 +0200

    Add missing migration

--- skillfarm/__init__.py
+++ skillfarm/__init__.py
@@ -2,7 +2,7 @@
 
 from skillfarm.app_settings import SKILLFARM_APP_NAME
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __title__ = "Skillfarm"
 
 USER_AGENT_TEXT = f"{SKILLFARM_APP_NAME} v{__version__}"
