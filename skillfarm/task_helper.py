a4acffa315ce66936b62270b3ab04051ddd3d6d9
Geuthur <geuthur@users.noreply.github.com>
2025-03-16 20:08:06 +0100

    [CHANGE] Update task scheduling and improve notification handling

--- skillfarm/task_helper.py
+++ skillfarm/task_helper.py
@@ -3,58 +3,14 @@ Skillfarm Helper
 """
 
 import time
-from functools import wraps
 
 from bravado.exception import HTTPNotModified
-from celery import signature
-from celery_once import AlreadyQueued
 
 from django.core.cache import cache
 
 from skillfarm.hooks import get_extension_logger
 
 logger = get_extension_logger(__name__)
-
-
-def enqueue_next_task(chain):
-    """
-    Queue next task, and attach the rest of the chain to it.
-    """
-    while len(chain):
-        _t = chain.pop(0)
-        _t = signature(_t)
-        _t.kwargs.update({"chain": chain})
-        try:
-            _t.apply_async(priority=9)
-        except AlreadyQueued:
-            # skip this task as it is already in the queue
-            logger.debug("Skipping task as its already queued %s", _t)
-            continue
-        break
-
-
-def no_fail_chain(func):
-    """
-    Decorator to chain tasks provided in the chain kwargs regardless of task failures.
-    """
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        excp = None
-        _ret = None
-        try:
-            _ret = func(*args, **kwargs)
-        except Exception as e:  # pylint: disable=broad-exception-caught
-            excp = e
-        finally:
-            _chn = kwargs.get("chain", [])
-            enqueue_next_task(_chn)
-            if excp:
-                raise excp
-        return _ret
-
-    return wrapper
-
 
 MAX_ETAG_LIFE = 60 * 60 * 24 * 7  # 7 Days
 
