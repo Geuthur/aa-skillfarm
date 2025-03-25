05a4a0280e4c31f82383ebe3a45404305ba03c02
Geuthur <geuthur@users.noreply.github.com>
2025-03-25 23:51:14 +0100

    [CHANGE] Update skillfarm_load_prices test

--- skillfarm/__init__.py
+++ skillfarm/__init__.py
@@ -2,7 +2,7 @@
 
 from skillfarm.app_settings import SKILLFARM_APP_NAME
 
-__version__ = "0.5.1"
+__version__ = "0.5.1.2"
 __title__ = "Skillfarm"
 
 USER_AGENT_TEXT = f"{SKILLFARM_APP_NAME} v{__version__}"
