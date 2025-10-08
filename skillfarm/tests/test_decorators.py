464cf4eecad17c80c14df031b7141378b73f465f
Geuthur <geuthur@users.noreply.github.com>
2025-10-08 08:52:01 +0200

    [CHANGE] Update Tests

--- skillfarm/tests/test_decorators.py
+++ skillfarm/tests/test_decorators.py
@@ -23,10 +23,24 @@ DECORATOR_PATH = "skillfarm.decorators."
 
 class TestDecorators(TestCase):
     @patch(DECORATOR_PATH + "fetch_esi_status")
+    @patch(DECORATOR_PATH + "IS_TESTING", new=True)
+    def test_when_esi_is_available_is_test(self, mock_fetch_esi_status):
+        """Test when ESI is available in testing mode."""
+
+        @when_esi_is_available
+        def trigger_esi_deco():
+            return "Testing Mode."
+
+        # when
+        result = trigger_esi_deco()
+        # then
+        self.assertEqual(result, "Testing Mode.")
+
+    @patch(DECORATOR_PATH + "fetch_esi_status")
     @patch(DECORATOR_PATH + "get_redis_client")
     @patch(DECORATOR_PATH + "IS_TESTING", new=False)
     def test_when_esi_is_ok(self, mock_get_redis_client, mock_fetch_esi_status):
-        # given
+        """Test when ESI is available in non-testing mode."""
         # make the redis client behave as if no cache is set and we can acquire the lock
         redis_client = MagicMock()
         redis_client.get.return_value = None
@@ -52,7 +66,7 @@ class TestDecorators(TestCase):
     @patch(DECORATOR_PATH + "get_redis_client")
     @patch(DECORATOR_PATH + "IS_TESTING", new=False)
     def test_when_esi_is_not_ok(self, mock_get_redis_client, mock_fetch_esi_status):
-        # given
+        """Test when ESI is not available in non-testing mode."""
         # make the redis client behave as if no cache is set and we can acquire the lock
         redis_client = MagicMock()
         redis_client.get.return_value = None
@@ -73,61 +87,12 @@ class TestDecorators(TestCase):
         self.assertIsNone(result)
 
     @patch(DECORATOR_PATH + "fetch_esi_status")
-    @patch(DECORATOR_PATH + "IS_TESTING", new=True)
-    def test_when_esi_is_available_is_test(self, mock_fetch_esi_status):
-        # given
-        @when_esi_is_available
-        def trigger_esi_deco():
-            return "Testing Mode."
-
-        # when
-        result = trigger_esi_deco()
-        # then
-        self.assertEqual(result, "Testing Mode.")
-
-    @patch(DECORATOR_PATH + "fetch_esi_status")
-    @patch(DECORATOR_PATH + "get_redis_client")
-    @patch(DECORATOR_PATH + "IS_TESTING", new=False)
-    def test_acquire_lock_or_wait_for_cache_when_cache_available(
-        self, mock_get_redis_client, mock_fetch_esi_status
-    ):
-        # given
-        # make the redis client behave as if cache is set
-        redis_client = MagicMock()
-        redis_client.get.return_value = "1"
-        mock_get_redis_client.return_value = redis_client
-
-        result, cache_available = acquire_lock_or_wait_for_cache(redis_client)
-        # then
-        self.assertFalse(result)
-        self.assertTrue(cache_available)
-
-    @patch(DECORATOR_PATH + "fetch_esi_status")
-    @patch(DECORATOR_PATH + "get_redis_client")
-    @patch(DECORATOR_PATH + "IS_TESTING", new=False)
-    def test_acquire_lock_or_wait_for_cache_when_lock_acquired(
-        self, mock_get_redis_client, mock_fetch_esi_status
-    ):
-        # given
-        # make the redis client behave as if no cache is set and we can acquire the lock
-        redis_client = MagicMock()
-        redis_client.get.return_value = None
-        redis_client.set.return_value = True
-        redis_client.setex.return_value = None
-        mock_get_redis_client.return_value = redis_client
-
-        result, cache_available = acquire_lock_or_wait_for_cache(redis_client)
-        # then
-        self.assertTrue(result)
-        self.assertFalse(cache_available)
-
-    @patch(DECORATOR_PATH + "fetch_esi_status")
     @patch(DECORATOR_PATH + "get_redis_client")
     @patch(DECORATOR_PATH + "IS_TESTING", new=False)
     def test_when_esi_is_ok_and_cached(
         self, mock_get_redis_client, mock_fetch_esi_status
     ):
-        # given
+        """Test when ESI is available in non-testing mode and cached."""
         redis_client = MagicMock()
         redis_client.get.return_value = "1"
         mock_get_redis_client.return_value = redis_client
@@ -144,6 +109,87 @@ class TestDecorators(TestCase):
         # then
         mock_fetch_esi_status.assert_not_called()
         self.assertEqual(result, "Esi is Available")
+
+    @patch(DECORATOR_PATH + "fetch_esi_status")
+    @patch(DECORATOR_PATH + "get_redis_client")
+    @patch(DECORATOR_PATH + "IS_TESTING", new=False)
+    def test_when_esi_is_ok_and_not_acquired(
+        self, mock_get_redis_client, mock_fetch_esi_status
+    ):
+        """Test when ESI is available in non-testing mode and not acquired."""
+        redis_client = MagicMock()
+        redis_client.get.return_value = None
+        # simulate that we did not acquire the lock so the wait runs
+        redis_client.set.return_value = False
+        redis_client.setex.return_value = None
+        mock_get_redis_client.return_value = redis_client
+        # ensure the fetch_esi_status call reports ESI as up
+        mock_fetch_esi_status.return_value.is_ok = True
+
+        @when_esi_is_available
+        def trigger_esi_deco():
+            return "Esi is Available"
+
+        # when
+        result = trigger_esi_deco()
+        # then
+        mock_fetch_esi_status.assert_called_once()
+        self.assertEqual(result, "Esi is Available")
+
+    @patch(DECORATOR_PATH + "fetch_esi_status")
+    @patch(DECORATOR_PATH + "get_redis_client")
+    @patch(DECORATOR_PATH + "IS_TESTING", new=False)
+    def test_wait_for_lock_or_cache_when_cache_available(
+        self, mock_get_redis_client, mock_fetch_esi_status
+    ):
+        """Test wait_for_lock_or_cache when cache is available."""
+        # make the redis client behave as if cache is set
+        redis_client = MagicMock()
+        redis_client.get.return_value = "1"
+        mock_get_redis_client.return_value = redis_client
+
+        result, cache_available = acquire_lock_or_wait_for_cache(redis_client)
+        # then
+        self.assertFalse(result)
+        self.assertTrue(cache_available)
+
+    @patch(DECORATOR_PATH + "fetch_esi_status")
+    @patch(DECORATOR_PATH + "get_redis_client")
+    @patch(DECORATOR_PATH + "IS_TESTING", new=False)
+    def test_wait_for_lock_or_cache_when_lock_acquired(
+        self, mock_get_redis_client, mock_fetch_esi_status
+    ):
+        """Test wait_for_lock_or_cache when lock is acquired."""
+        # make the redis client behave as if no cache is set and we can acquire the lock
+        redis_client = MagicMock()
+        redis_client.get.return_value = None
+        redis_client.set.return_value = True
+        redis_client.setex.return_value = None
+        mock_get_redis_client.return_value = redis_client
+
+        result, cache_available = acquire_lock_or_wait_for_cache(redis_client)
+        # then
+        self.assertTrue(result)
+        self.assertFalse(cache_available)
+
+    @patch(DECORATOR_PATH + "fetch_esi_status")
+    @patch(DECORATOR_PATH + "get_redis_client")
+    @patch(DECORATOR_PATH + "IS_TESTING", new=False)
+    def test_wait_for_lock_or_cache_redis_exception(
+        self, mock_get_redis_client, mock_fetch_esi_status
+    ):
+        """Test wait_for_lock_or_cache when Redis raises exceptions."""
+        # make the redis client behave as if no cache is set and we can acquire the lock
+        redis_client = MagicMock()
+        redis_client.get.return_value = None
+        redis_client.set.side_effect = Exception("Simulated Redis Error")
+        redis_client.setex.return_value = None
+        mock_get_redis_client.return_value = redis_client
+
+        result, cache_available = acquire_lock_or_wait_for_cache(redis_client)
+        # then
+        self.assertFalse(result)
+        self.assertFalse(cache_available)
 
     def test_log_timing(self):
         # given
